2043ab6228172b7b3c050d514f5e7e7d83a0a21a
Piotr Rybka <piotr.rybka82@gmail.com>
2021-02-02 20:49:02 +0100

    initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Vector3 offset;
+
+    public GameObject player;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ustawiam wyjsciowy offset na pozycje w relacji do pozycji gracza
+        offset = transform.position - player.transform.position;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //przesuwam kamere względem gracza
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/DoorControler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControler : MonoBehaviour
+{
+    const string openDoorParameterName = "openDoor";
+    const string closeDoorParameterName = "closeDoor";
+
+    public Transform Door;
+
+    Animator _doorAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _doorAnimator = Door.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        _doorAnimator.SetBool(openDoorParameterName, true);
+    }
+
+    void OnTriggerExit(Collider other) 
+    {
+        _doorAnimator.SetBool(openDoorParameterName, false);
+    }
+}
